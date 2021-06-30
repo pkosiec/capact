9938b6fc2f52e7cca6353ace2ebab0419411cde1
Paweł Kosiec <pawel.kosiec@gmail.com>
2021-06-30 12:14:58 +0200

    Test PR

--- hack/dev-cluster-create.sh
+++ hack/dev-cluster-create.sh
@@ -15,6 +15,8 @@ REPO_ROOT_DIR=$(cd "${CURRENT_DIR}/.." && pwd)
 readonly CURRENT_DIR
 readonly REPO_ROOT_DIR
 
+# test
+
 # shellcheck source=./hack/lib/utilities.sh
 source "${CURRENT_DIR}/lib/utilities.sh" || { echo 'Cannot load CI utilities.'; exit 1; }
 # shellcheck source=./hack/lib/const.sh
